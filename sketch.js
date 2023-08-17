1c3ec538edcecd2ff25077e08e6061c5a4b8e1ff
Kotuseid <sbcproductionsofficial@gmail.com>
2023-08-17 19:43:48 +1000

    added health boost

--- sketch.js
+++ sketch.js
@@ -7,7 +7,8 @@ let player1, player2,
     backgroundImage,
     border,
     digitalDisco,
-    maxCooldown = 150;
+    maxCooldown = 150,
+    healthBoost;
 
 //preload images, fonts, animations, soundtracks
 
@@ -177,8 +178,27 @@ function setup() {
     player2.health = 100;
     player2.cooldown = 0;
 
-
     player1.overlaps(player2);
+
+
+    healthBoost = new Group();
+    healthBoost.img = "assets/buffs/Life.png";
+    healthBoost.x = () => { return random(5, canvas.w - 5) };
+    healthBoost.y = () => { return random(canvas.w * 0.135, canvas.h - canvas.w * 0.07) };
+    healthBoost.d = 140;
+    healthBoost.scale = 0.4;
+    healthBoost.frequency = 5;//seconds
+
+    setInterval(() => {
+        if (healthBoost.length == 0) {
+            new healthBoost.Sprite();
+        }
+    }, healthBoost.frequency * 1000);
+
+    player1.overlaps(healthBoost, boostHealth);
+
+    player2.overlaps(healthBoost, boostHealth);
+
     //only for testing
     allSprites.debug = true;
 }
@@ -228,83 +248,4 @@ function draw() {
     //decrement the cooldown
     if (player1.cooldown > 0) player1.cooldown--;
     if (player2.cooldown > 0) player2.cooldown--;
-}
-
-//custom player stats display function
-function statsDisplay(x, y, w, h, sprite, name) {
-
-    let p = 5;
-
-    textAlign(CENTER, CENTER);
-    textFont(digitalDisco);
-    textSize(p * 4);
-
-    //display name
-    fill('black')
-    text(name.toUpperCase() + ': ' + sprite.health + '%', x + w / 2, y - 3 * p);
-
-    //display health bar
-    noStroke();
-    fill('black');
-    rect(x, y, w, h);
-    fill('red');
-    rect(x + p, y + p, (w - p * 2) * sprite.health / 100, h - 2 * p);
-    fill('white');
-    text("HEALTH", x + w / 2, y + h / 2);
-
-    //display cooldown bar
-    fill('black');
-    rect(x, y + h + p, w, h);
-    fill('green');
-    rect(x + p, y + h + 2 * p, (w - 2 * p) * (maxCooldown - sprite.cooldown) / maxCooldown, h - 2 * p);
-    fill('white');
-    text("COOLDOWN", x + w / 2, y + h / 2 + h + p);
-
-}
-
-//custom movement function
-function movement(sprite) {
-    let key = sprite.keys;
-
-    //move sprite horizontally
-    if (kb.pressing(key.right)) {
-        sprite.velocity.x = sprite.agility;
-
-    } else if (kb.pressing(key.left)) {
-        sprite.velocity.x = -sprite.agility;
-
-    } else {
-        sprite.velocity.x = 0;
-    }
-
-    //move sprite vertically
-    if (kb.pressing(key.up)) {
-        sprite.velocity.y = -sprite.agility;
-
-    } else if (kb.pressing(key.down)) {
-        sprite.velocity.y = sprite.agility;
-
-    } else {
-        sprite.velocity.y = 0;
-    }
-
-    //animate sprite walking or idle of not attacking or dead
-    if (sprite.ani.name != 'attack1' && sprite.ani.name != 'attack2' && sprite.ani.name != 'dead') {
-
-        if (kb.pressing(key.right)) {
-            sprite.changeAni('walk')
-            sprite.mirror.x = false;
-        } else if (kb.pressing(key.left)) {
-            sprite.changeAni('walk');
-            sprite.mirror.x = true;
-        }
-
-        if (kb.pressing(key.up) || kb.pressing(key.down)) {
-            sprite.changeAni('walk');
-        }
-
-        if (!kb.pressing(key.right) && !kb.pressing(key.left) && !kb.pressing(key.up) && !kb.pressing(key.down)) {
-            sprite.changeAni('idle');
-        }
-    }
 }
---
+++ util.js
@@ -0,0 +1,89 @@
+
+//custom player stats display function
+function statsDisplay(x, y, w, h, sprite, name) {
+
+    let p = 5;
+
+    textAlign(CENTER, CENTER);
+    textFont(digitalDisco);
+    textSize(p * 4);
+
+    //display name
+    fill('black')
+    text(name.toUpperCase() + ': ' + sprite.health + '%', x + w / 2, y - 3 * p);
+
+    //display health bar
+    noStroke();
+    fill('black');
+    rect(x, y, w, h);
+    fill('red');
+    rect(x + p, y + p, (w - p * 2) * sprite.health / 100, h - 2 * p);
+    fill('white');
+    text("HEALTH", x + w / 2, y + h / 2);
+
+    //display cooldown bar
+    fill('black');
+    rect(x, y + h + p, w, h);
+    fill('green');
+    rect(x + p, y + h + 2 * p, (w - 2 * p) * (maxCooldown - sprite.cooldown) / maxCooldown, h - 2 * p);
+    fill('white');
+    text("COOLDOWN", x + w / 2, y + h / 2 + h + p);
+
+}
+
+//custom movement function
+function movement(sprite) {
+    let key = sprite.keys;
+
+    //move sprite horizontally
+    if (kb.pressing(key.right)) {
+        sprite.velocity.x = sprite.agility;
+
+    } else if (kb.pressing(key.left)) {
+        sprite.velocity.x = -sprite.agility;
+
+    } else {
+        sprite.velocity.x = 0;
+    }
+
+    //move sprite vertically
+    if (kb.pressing(key.up)) {
+        sprite.velocity.y = -sprite.agility;
+
+    } else if (kb.pressing(key.down)) {
+        sprite.velocity.y = sprite.agility;
+
+    } else {
+        sprite.velocity.y = 0;
+    }
+
+    //animate sprite walking or idle of not attacking or dead
+    if (sprite.ani.name != 'attack1' && sprite.ani.name != 'attack2' && sprite.ani.name != 'dead') {
+
+        if (kb.pressing(key.right)) {
+            sprite.changeAni('walk')
+            sprite.mirror.x = false;
+        } else if (kb.pressing(key.left)) {
+            sprite.changeAni('walk');
+            sprite.mirror.x = true;
+        }
+
+        if (kb.pressing(key.up) || kb.pressing(key.down)) {
+            sprite.changeAni('walk');
+        }
+
+        if (!kb.pressing(key.right) && !kb.pressing(key.left) && !kb.pressing(key.up) && !kb.pressing(key.down)) {
+            sprite.changeAni('idle');
+        }
+    }
+}
+
+function boostHealth(p, h) {
+
+    p.health += 5;
+    if (p.health > 100) {
+        p.health = 100;
+    }
+    h.remove();
+
+}
